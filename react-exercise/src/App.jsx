f096a4c67ad5da805883ab020057d496ec7f881b
Suleyman <suleymanks@live.com>
2023-10-11 10:30:06 +0300

    ex04

--- react-exercise/src/App.jsx
+++ react-exercise/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 
 function App() {
+
   const SayHello = ({ name }) => {
     return <h1>Hello, {name}</h1>
   }
 
+  const JSXExpression = <p>"Suleyman"</p>
+
   return (
     <>
       <h1>Hello, world!</h1>
-      <SayHello />
+      <SayHello name={JSXExpression}/>
     </>
   )
 }
