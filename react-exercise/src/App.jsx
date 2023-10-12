ddd9b66fe39253f2ed2ec6d4e021f2e370415b9d
Suleyman <suleymanks@live.com>
2023-10-12 22:24:29 +0300

    07

--- react-exercise/src/App.jsx
+++ react-exercise/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import Hello from "./components/Hello"
 
 function App() {
 
 
   return (
-    <>
-    </>
-  )
+    <div> 
+    <Hello /> 
+    </div>
+  );
 }
 
 export default App
---
+++ react-exercise/src/components/Hello.jsx
@@ -0,0 +1,5 @@
+const Hello = () => {
+    return <h2>Hello, World!</h2>
+}
+
+export default Hello;
