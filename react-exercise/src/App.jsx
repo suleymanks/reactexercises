346baf868be08d138e694f42a126f7c83e816cd7
Suleyman <suleymanks@live.com>
2023-10-12 18:55:37 +0300

    05-jsx

--- react-exercise/src/App.jsx
+++ react-exercise/src/App.jsx
@@ -2,19 +2,9 @@ import './App.css'
 
 function App() {
 
-  const Function = (a, b) => {
-    const sum = a + b;
-    return (
-    <>
-      Sum: {sum}
-    </>
-  )  
-  }
 
-  
   return (
     <>
-      <h2>{Function(3,5)}</h2>
     </>
   )
 }
